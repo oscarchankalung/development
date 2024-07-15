8533c0fbdcaa68e5b0062dc20a45da0ed5c5ee7d
Oscar Chan <oscarchankalung@gmail.com>
2024-07-15 05:59:06 -0400

    update

--- 02 Interviews/202407 Recurse Center/Code/TicTacToe_N.py
+++ 02 Interviews/202407 Recurse Center/Code/TicTacToe_N.py
@@ -10,15 +10,14 @@ gridRow = "-" * (gridNum * 2 - 1)
 
 
 def buildGrid():
-    # [ [1,2,3], [4,5,6], [7,8,9] ]
     map = []
 
     for i in range(gridNum):
         row = []    
         for j in range(gridNum):
-            x = i * gridNum # 0,3,6
-            y = j + 1       # 1,2,3
-            value = x + y
+            x = i * gridNum # [0, 3, 6]
+            y = j + 1       # [1, 2, 3]
+            value = x + y   # [1, 2, 3] > [4, 5, 6] > [7, 8, 9]
             row.append(value)
         map.append(row)
     return map
@@ -36,8 +35,45 @@ def printGrid():
 # def placeMark():
 #     return
 
-# def checkGrid():
-#     return
+def checkGrid():
+    
+    def checkValues(values = []):
+        if len(values) == 0:
+            return False
+
+        for i in range(1, len(values)):
+            prevValue = values[i - i] # [0, 1]
+            currValue = values[i]     # [1, 2]
+            if prevValue != currValue:
+                return False
+
+        return True
+
+    for i in range(gridNum):
+        horzValues = []
+        vertValues = []
+        diagValues = []
+
+        # [00, 01, 02]
+        # [10, 11, 12]
+        # [20, 21, 22]
+        for j in range(gridNum):
+            horzValues.append(grid[i][j]) # [00, 01, 02]
+            vertValues.append(grid[j][i]) # [00, 10, 20]
+
+            if i == 0:
+                diagValues.append(grid[i+j][j]) # [00, 11, 22]
+            if i == gridNum - 1:
+                diagValues.append(grid[j][i-j]) # [02, 12, 20]
+        
+        if checkValues(horzValues):
+            return True
+        if checkValues(vertValues):
+            return True
+        if checkValues(diagValues):
+            return True
+
+    return False
 
 # def startGame():
 #     return
